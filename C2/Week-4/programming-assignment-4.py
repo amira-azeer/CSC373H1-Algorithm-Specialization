68e3189e28c4e74d6d2166476ae304f37485ce4b
Amira Azeer <103902057+amira-azeer@users.noreply.github.com>
2026-05-11 11:53:37 +0530

    Update programming-assignment-4.py

--- C2/Week-4/programming-assignment-4.py
+++ C2/Week-4/programming-assignment-4.py
@@ -1,1 +1,21 @@
+# Two-Sum Algorithm
+from collections import defaultdict
 
+with open('algo1-programming_prob-2sum.txt') as f:
+    data = f.readlines()
+
+nums = defaultdict(list)
+for line in data:
+    if num := line.strip():
+        n = int(num)
+        nums[n // 10000].append(n)
+
+res = set()
+for key1, xs in list(nums.items()):
+    for key2 in range(-key1 - 2, -key1 + 1):
+        for x in xs:
+            for y in nums.get(key2, []):
+                if abs(x + y) < 10000:
+                    res.add(x + y)
+
+print(len(res))
